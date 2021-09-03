22d58f5fa0c795898c3b479cd6349ebce0419768
ゆう <53101647+yuu5733@users.noreply.github.com>
2021-09-03 13:17:08 +0900

    GraphQL Code Generator and Apollo Client setting finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { Container, Grid } from "@material-ui/core";
 import { VideoCard } from "src/components/VideoCard";
 
--- src/templates/DashboardHeader/index.tsx
+++ src/templates/DashboardHeader/index.tsx
@@ -1,14 +1,33 @@
-import { AppBar, Avatar, IconButton, Toolbar } from "@material-ui/core";
+import { useEffect } from "react";
+import {
+  AppBar,
+  Avatar,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 import { Logo } from "src/components/Logo";
 import { SearchBar } from "src/templates/DashboardHeader/SearchBar/index";
+// codegenで生成したコードをimport
+import { useUserByIdQuery } from "src/utils/graphql/generated";
 
 import useStyles from "src/templates/DashboardHeader/styles";
 
 export const DashboardHeader = () => {
   // CSSを生成
   const styles = useStyles();
+
+  //GraphQLの`Query`を発行して、Hasuraのエンドポイントにリクエストを飛ばし、返り値を取得
+  const { data, error } = useUserByIdQuery({
+    variables: { id: "testid" },
+  });
+
+  useEffect(() => {
+    console.log(data);
+    console.log(error);
+  }, [data]);
 
   return (
     <AppBar elevation={0} color="inherit">
@@ -25,6 +44,11 @@ export const DashboardHeader = () => {
         <SearchBar />
         
         <div className={styles.flex}>
+          {/* `data`内に`Schema`と同じ名前のオブジェクトのデータが格納されている */}
+          <IconButton>
+            <Typography>{data?.users_by_pk?.name}</Typography>
+          </IconButton>
+
           <IconButton>
             <VideoCallIcon />
           </IconButton>
